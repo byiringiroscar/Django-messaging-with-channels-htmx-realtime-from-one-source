3805b3640614e42b26c606830cc57e3cef1b5c0d
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-10 13:39:47 +0200

    Add ind user messages

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 
 # Create your views here.
@@ -8,3 +8,9 @@ def index(request):
     user_list = User.objects.all()
     context = {"users": user_list}
     return render(request, "index.html", context)
+
+
+def user_message(request, id):
+    single_user = get_object_or_404(User, pk=id)
+    context = {"user": single_user}
+    return render(request, "user_message.html", context)
